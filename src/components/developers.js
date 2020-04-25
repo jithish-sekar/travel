ba0a3452f1efdc01299dbfee9a47fba66e7dd394
jithish-sekar <63664628+jithish-sekar@users.noreply.github.com>
2020-04-25 10:15:38 +0530

    css

--- src/components/developers.js
+++ src/components/developers.js
@@ -13,46 +13,41 @@ class Developer extends Component {
                 </center>
                 <br /><br />
                 <center>
-                    <div className='team-area'>
-                        <div className='single-team'>
+                    <div className='box'>
+                        <div className='imgBx'>
                             <img
                                 className='devimg'
                                 src='jithu6.jpg' />
-                            <div className='team-text'></div>
-                            <h2>Jithish</h2>
-                            <p>Full Stack Developer</p>
-                            <p>
-                                <a href='https://www.facebook.com/profile.php?id=100012662224779" rel="noopener noreferrer" target="_blank"' > <i class="fa fa-facebook-square"></i></a><span></span>
-                                <a href='https://www.linkedin.com/in/jithish-sekar-a46a95157?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bh8jOfTQUSlWqq9PhJ2Y2Wg%3D%3D" rel="noopener noreferrer" target="_blank" ' > <i class="fa fa-linkedin-square"></i></a><span></span>
-                                <a href='https://twitter.com/JithuSekar " rel="noopener noreferrer" target="_blank"'><i class="fa fa-twitter-square"></i></a>
-                                <a href='https://github.com/jithish-sekar'><i class="fa fa-github-square"></i></a>
-                                <a href='https://github.com/jithish-sekar'><i class='fa fa-behance-square " rel="noopener noreferrer" target="_blank"'></i></a>
-                            </p>
-
                         </div>
-
-                        <div className='single-team'>
+                        <ul className="social-icon">
+                            <li><a href='https://www.facebook.com/profile.php?id=100012662224779" rel="noopener noreferrer" target="_blank"' > <i class="fa fa-facebook-square"></i></a><span></span></li>
+                            <li><a href='https://www.linkedin.com/in/jithish-sekar-a46a95157?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bh8jOfTQUSlWqq9PhJ2Y2Wg%3D%3D" rel="noopener noreferrer" target="_blank" ' > <i class="fa fa-linkedin-square"></i></a><span></span></li>
+                            <li><a href='https://twitter.com/JithuSekar " rel="noopener noreferrer" target="_blank"'><i class="fa fa-twitter-square"></i></a></li>
+                            <li><a href='https://github.com/jithish-sekar'><i class="fa fa-github-square"></i></a></li>
+                            <li><a href='https://github.com/jithish-sekar'><i class='fa fa-behance-square " rel="noopener noreferrer" target="_blank"'></i></a></li>
+                        </ul>
+                    </div><br/><br/>
+                    <div className='box'>
+                        <div className='imgBx'>
                             <img
-                                className="devimg"
+                                className='devimg'
                                 src='dhanush1.jpg' />
-                            <div className='team-text'></div>
-                            <h2>Dhanush</h2>
-                            <p>Full Stack Developer</p>
-                            <p>
-                                <a href='https://www.facebook.com/profile.php?id=100012662224779" rel="noopener noreferrer" target="_blank"' > <i class="fa fa-facebook-square"></i></a><span></span>
-                                <a href='https://www.linkedin.com/in/jithish-sekar-a46a95157?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bh8jOfTQUSlWqq9PhJ2Y2Wg%3D%3D" rel="noopener noreferrer" target="_blank" ' > <i class="fa fa-linkedin-square"></i></a><span></span>
-                                <a href='https://twitter.com/JithuSekar " rel="noopener noreferrer" target="_blank"'><i class="fa fa-twitter-square"></i></a>
-                                <a href='https://github.com/jithish-sekar'><i class="fa fa-github-square"></i></a>
-                                <a href='https://github.com/jithish-sekar'><i class='fa fa-behance-square " rel="noopener noreferrer" target="_blank"'></i></a>
-                            </p>
-
                         </div>
+                        <ul className="social-icon">
+                            <li><a href='https://www.facebook.com/profile.php?id=100012662224779" rel="noopener noreferrer" target="_blank"' > <i class="fa fa-facebook-square"></i></a><span></span></li>
+                            <li><a href='https://www.linkedin.com/in/jithish-sekar-a46a95157?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bh8jOfTQUSlWqq9PhJ2Y2Wg%3D%3D" rel="noopener noreferrer" target="_blank" ' > <i class="fa fa-linkedin-square"></i></a><span></span></li>
+                            <li><a href='https://twitter.com/JithuSekar " rel="noopener noreferrer" target="_blank"'><i class="fa fa-twitter-square"></i></a></li>
+                            <li><a href='https://github.com/jithish-sekar'><i class="fa fa-github-square"></i></a></li>
+                            <li><a href='https://github.com/jithish-sekar'><i class='fa fa-behance-square " rel="noopener noreferrer" target="_blank"'></i></a></li>
+                        </ul>
                     </div>
+                    <br />
+                
+                    
+                    
                 </center>
-
-
-
             </div>
+
         )
     }
 }
